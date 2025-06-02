c0d88b4548f290cc39aa27c87148420bf61f0eba
SSW4111 <sswoo4111@gmail.com>
2025-06-02 14:36:53 +0900

    최초 프로젝트 생성

---
+++ project911/src/main/java/com/jw/project911/Project911Application.java
@@ -0,0 +1,13 @@
+package com.jw.project911;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project911Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project911Application.class, args);
+	}
+//생성
+}
